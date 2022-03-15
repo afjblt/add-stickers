35e8d9066b55deeecc12be85ddef891cd906ffaa
FernandoDev02 <dev02@rj-tecnologia.com>
2022-03-15 12:19:07 -0400

    Faltando pagina de checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Footer from './components/Footer';
 import Form from './components/Form';
 import Header from './components/Header';
+import FormStickers from "./components/pages/FormStickers";
 
 function App() {
   return (
-    <div className="container">
-      <div className='box'>
-        <Header />
-        <Form />
-        <Footer />
+    <Router>
+      <div className="container">
+        <div className='box'>
+          <Routes>
+            <Route exact path='/' element={<FormStickers />}></Route>
+          </Routes>
+        </div>
       </div>
-     
-    </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/pages/FormStickers.jsx
@@ -0,0 +1,15 @@
+import Footer from "../Footer";
+import Form from "../Form";
+import Header from "../Header";
+
+function FormStickers() {
+    return ( 
+        <>
+            <Header />
+            <Form />
+            <Footer />
+        </>
+     );
+}
+
+export default FormStickers;
